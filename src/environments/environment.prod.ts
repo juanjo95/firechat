ba561fc8c75f0c2dbd2bcce8304bf86ab51bcda4
Juan J Salgado <zallagox95@hotmail.com>
2021-12-21 00:02:37 -0500

    Bug solucionado cambios en archivos para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDNsR2J3IoD9cHzvsrXhdWG0BD4YqKdqlU",
+    authDomain: "firechat-b5c96.firebaseapp.com",
+    projectId: "firechat-b5c96",
+    storageBucket: "firechat-b5c96.appspot.com",
+    messagingSenderId: "482829919391",
+    appId: "1:482829919391:web:7b11e5aa72388b97e02eaa",
+    measurementId: "G-PHXNB5XVV7"
+  }
 };
